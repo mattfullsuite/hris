c2b96e0807ff095a5f02f8a4c518657a844a60ea
marco_antero <marco@fullsuite.ph>
2023-11-29 13:33:55 +0800

    added new manager component

---
+++ frontend/src/components/manager/ManagerPTORequestTable.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ManagerPTORequestTable = () => {
+    return(
+        <>
+      
+    </>
+    )
+}
+ export default ManagerPTORequestTable;
--- frontend/src/pages/leads/LeadPTORequests.jsx
+++ frontend/src/pages/leads/LeadPTORequests.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ManagerSideBar from "../../components/manager/ManagerSideBar";
+import ManagerPTORequestTable from "../../components/manager/ManagerPTORequestTable";
 
 const LeadPTORequest = () => {
     return(
         <>
         <ManagerSideBar></ManagerSideBar>
+        <ManagerPTORequestTable></ManagerPTORequestTable>
         </>
     )
 }
