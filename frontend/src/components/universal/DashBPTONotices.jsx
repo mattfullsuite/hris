5eb4a782251a185142b7a9c46b6f8e87a94eb1a4
Marvin Bautista <marvin@fullsuite.ph>
2023-12-07 14:15:03 +0800

    structuring data table for approved leaves

--- frontend/src/components/universal/DashBPTONotices.jsx
+++ frontend/src/components/universal/DashBPTONotices.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import moment from "moment";
+import DataTable from "react-data-table-component";
 
 const DashBPTONotices = () => {
   var count = 1;
@@ -19,11 +20,32 @@ const DashBPTONotices = () => {
     fetchAllApproved();
   }, []);
 
+  const columns = [
+    {
+      name: "#",
+      selector: row => row.leave_id,
+      sortable: true
+    },
+
+    {
+      name: "Date filed",
+      selector: row => moment(row.date_filed).format('MMM DD YYYY')
+    }
+  ]
+
   return (
     <>
       {/* PTO Notices */}
       <div className="m-2 p-3 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 flex flex-col items-center justify-center">
         <h1 className="text-lg font-semibold">Approved PTO Notices</h1>
+
+
+        <DataTable
+          column = {columns}
+          data = {approved}
+          pagination
+        ></DataTable>
+
         <div className="overflow-x-auto max-w-full">
           <table className="table">
             {/* head */}
