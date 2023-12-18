6ed9dc617efae20f4e46baf88fa4504525a7b576
Marvin Bautista <marvin@fullsuite.ph>
2023-12-18 15:28:09 +0800

    modification of approved andd own db and lead dir.

--- frontend/src/pages/leads/LeadDirectory.jsx
+++ frontend/src/pages/leads/LeadDirectory.jsx
@@ -2,47 +2,75 @@ import react, { useState, useEffect } from "react";
 import axios from "axios";
 import ManagerSideBar from "../../components/manager/ManagerSideBar";
 import Headings from "../../components/universal/Headings";
+import EmployeeDirectoryCard from "../../components/universal/EmployeeDirectoryCard";
 
 const LeadDirectory = () => {
+  const [corpStrat, setCorpStrat] = useState([]);
+  const [ventures, setVenture] = useState([]);
 
-    const [ventures, setVenture] = useState([])
+  useEffect(() => {
+    const setData = async () => {
+      try {
+        const venture = await axios.get(
+          "http://localhost:6197/getVentureDivision"
+        );
+        const corp = await axios.get(
+          "http://localhost:6197/getCorpStratDivision"
+        );
 
-    useEffect(() => {
-        const setData = async () => {
-            try {
-                const venture = await axios.get("http://localhost:6197/getVentureDivision")
+        setCorpStrat(corp.data);
+        setVenture(venture.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
 
-                setVenture(venture.data)
-            } catch(e) {
-                console.log(e)
-            }
-        }
+    setData();
+  });
 
-        setData();
-    })
-  
-    return (
+  return (
     <>
       <ManagerSideBar></ManagerSideBar>
 
       <div className="p-4 sm:ml-64 flex flex-col">
         <Headings text={"Employee Directory"} />
 
-        <div className="mt-24">
+        <div className="mt-24 flex flex-col gap-40">
+          <div>
+            <h1 className="text-3xl font-bold text-center">
+              Corporate Strategy
+            </h1>
+
+            <div className="flex flex-row justify-center items-center gap-4 flex-wrap mt-10">
+              {corpStrat.map((c) => (
+                <EmployeeDirectoryCard
+                  image={c.emp_pic}
+                  firstName={c.f_name}
+                  lastName={c.s_name}
+                  department={c.dept_name}
+                  position={c.position_name}
+                  workEmail={c.work_email}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div>
             <h1 className="text-3xl font-bold text-center">Venture Capital</h1>
 
             <div className="flex flex-row justify-center items-center gap-4 flex-wrap mt-10">
-                {ventures.map((v) => (
-                    <div className="bg-gray-200 flex flex-col justify-center items-center p-5 w-60">
-                        {(v.emp_pic == "" || v.emp_pic == null) ? <div className="h-24 w-24 bg-gray-500 rounded-full flex justify-center items-center text-4xl text-white font-medium">{v.f_name.charAt(0) + v.s_name.charAt(0)}</div> : <img className="h-24 w-24 rounded-full"/>}
-
-                        <p>{v.f_name + " " + v.s_name}</p>
-                        <p>{v.position_name}</p>
-                        <p>{v.dept_name} Department</p>
-                        
-                    </div>
-                ))}
+              {ventures.map((v) => (
+                <EmployeeDirectoryCard
+                  image={v.emp_pic}
+                  firstName={v.f_name}
+                  lastName={v.s_name}
+                  department={v.dept_name}
+                  position={v.position_name}
+                  workEmail={v.work_email}
+                />
+              ))}
             </div>
+          </div>
         </div>
       </div>
     </>
