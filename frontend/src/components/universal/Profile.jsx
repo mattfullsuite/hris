ffe6bb51fcd5fc48ab0e76cd556759975d50b86e
marco_antero <marco@fullsuite.ph>
2023-12-21 17:39:00 +0800

    modified profile

--- frontend/src/components/universal/Profile.jsx
+++ frontend/src/components/universal/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = () => {
     emergency_contact_name: "",
     emergency_contact_num: "",
     civil_status: "",
-  })
+  });
   const [visible, setVisible] = useState(false);
   const [visible2, setVisible2] = useState(true);
 
@@ -20,13 +20,14 @@ const Profile = () => {
       try {
         const res = await Axios.get("http://localhost:6197/myProfile");
         setProfile(res.data);
-        setNewInfo({...newInfo,
+        setNewInfo({
+          ...newInfo,
           personal_email: res.data[0].personal_email,
           contact_num: res.data[0].contact_num,
           emergency_contact_name: res.data[0].emergency_contact_name,
           emergency_contact_num: res.data[0].emergency_contact_num,
-          civil_status: res.data[0].civil_status, 
-        })
+          civil_status: res.data[0].civil_status,
+        });
       } catch (err) {
         console.log(err);
       }
@@ -35,39 +36,37 @@ const Profile = () => {
   }, []);
 
   const enableFields = (event) => {
-    if (event.currentTarget.id === "edit-button"){
-      setVisible(true)
-      setVisible2(false)
-      document.getElementById("personal_email").disabled=false;
-      document.getElementById("contact_num").disabled=false;
-      document.getElementById("emergency_contact_name").disabled=false;
-      document.getElementById("emergency_contact_num").disabled=false;
-      document.getElementById("civil_status").disabled=false;
+    if (event.currentTarget.id === "edit-button") {
+      setVisible(true);
+      setVisible2(false);
+      document.getElementById("personal_email").disabled = false;
+      document.getElementById("contact_num").disabled = false;
+      document.getElementById("emergency_contact_name").disabled = false;
+      document.getElementById("emergency_contact_num").disabled = false;
+      document.getElementById("civil_status").disabled = false;
     }
-  }
+  };
 
   const disableFields = (event) => {
-    if (event.currentTarget.id === "save-button"){
-      setVisible(false)
-      setVisible2(true)
-      document.getElementById("personal_email").disabled=true;
-      document.getElementById("contact_num").disabled=true;
-      document.getElementById("emergency_contact_name").disabled=true;
-      document.getElementById("emergency_contact_num").disabled=true;
-      document.getElementById("civil_status").disabled=true;
+    if (event.currentTarget.id === "save-button") {
+      setVisible(false);
+      setVisible2(true);
+      document.getElementById("personal_email").disabled = true;
+      document.getElementById("contact_num").disabled = true;
+      document.getElementById("emergency_contact_name").disabled = true;
+      document.getElementById("emergency_contact_num").disabled = true;
+      document.getElementById("civil_status").disabled = true;
     }
-  }
+  };
 
   const handleChange = (event) => {
-    setNewInfo({...newInfo,[event.target.name]: [event.target.value]});
+    setNewInfo({ ...newInfo, [event.target.name]: [event.target.value] });
     console.log(JSON.stringify(newInfo));
-  }
-  
-  const saveProfile = (event) => {
-    event.preventDefault()
+  };
 
-    
-  }
+  const saveProfile = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
@@ -128,7 +127,6 @@ const Profile = () => {
                     dataSlot="icon"
                     className="w-5 h-5"
                   >
-                    
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -162,55 +160,31 @@ const Profile = () => {
             </div>
           </div>
 
-          {visible2 &&
-          <div className="ml-1 mt-10">
-            <button 
-            id="edit-button"
-            className="btn btn-sm btn-outline normal-case mx-1"
-            onClick={enableFields}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-6 h-6"
+          {visible2 && (
+            <div className="mx-1 mt-4 flex justify-end">
+            <button
+                id="edit-button"
+                className="btn btn-sm btn-outline normal-case mx-1"
+                onClick={enableFields}
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                />
-              </svg>
-              Edit
-            </button>
-          </div>
-          }
-
-          {visible &&
-          <div className="ml-1 mt-10">
-            <button 
-            id="save-button"
-            className="btn btn-sm btn-outline normal-case mx-1"
-            onClick={disableFields}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                />
-              </svg>
-              Save
-            </button>
-          </div>}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
+                </svg>
+                Edit
+              </button>
+            </div>
+          )}
 
           {/* Contact Information */}
           <div className="m-2 p-3 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 flex flex-1 flex-col">
@@ -426,6 +400,34 @@ const Profile = () => {
               <div className="flex-1"></div> */}
             {/* </div> */}
           </div>
+          {visible && (
+            <div className="mx-1 mt-4 flex justify-end">
+              <div className="">
+                <button
+                  id="save-button"
+                  className="btn btn-sm btn-outline normal-case mx-1"
+                  onClick={disableFields}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    data-slot="icon"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12"
+                    />
+                  </svg>
+                  Save
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       ))}
     </>
